fc8f526eba1b9b5a4801b8365f9d88fddfde978d
Nurlytan <nurik.bro.2000@mail.ru>
2023-05-11 16:32:13 +0600

    Admin Contacts

---
+++ app/Http/Controllers/ContactsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Contact;
+
+class ContactsController extends Controller
+{
+
+    public function saveContacts(Request $request){
+        // return $request;
+        $phone_number = $request->phone_number;
+        $email = $request->email;
+        $address = $request->address;
+
+        $contact = Contact::findById(1);
+        $contact->update([
+            'phone_number' => $phone_number,
+            'email' => $email,
+            'address' => $address
+        ]);
+        return response()->json(['contacts' => $contact]);
+    }
+
+    public function getContacts(){
+        return response()->json(['contacts' => Contact::all()->first()]);
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -38,7 +38,7 @@ Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:api')
 // Route::post('add-page', "App\Http\Controllers\PageController@addPage")->middleware('auth:api');
 // Route::post('update-page', "App\Http\Controllers\PageController@updatePage")->middleware('auth:api');
 // Route::post('delete-page', "App\Http\Controllers\PageController@deletePage")->middleware('auth:api');
-// Route::post('save-contacts', "App\Http\Controllers\ContactsController@saveContacts")->middleware('auth:api');
+Route::post('save-contacts', "App\Http\Controllers\ContactsController@saveContacts")->middleware('auth:api');
 Route::post('save-main-page', "App\Http\Controllers\MainPageController@saveMainPage")->middleware('auth:api');
 // Route::post('save-admin', "App\Http\Controllers\AdminController@saveAdmin")->middleware('auth:api');
 
@@ -52,7 +52,7 @@ Route::post('get-main-page', "App\Http\Controllers\MainPageController@getMainPag
 // Route::get('get-category-images', "App\Http\Controllers\ImageController@getCategoryImages");
 // Route::post('get-likes', "App\Http\Controllers\ImageController@getLikes");
 // Route::post('get-images-admin', "App\Http\Controllers\ImageController@getImagesAdmin")->middleware('auth:api');
-// Route::post('get-contacts', "App\Http\Controllers\ContactsController@getContacts");
+Route::post('get-contacts', "App\Http\Controllers\ContactsController@getContacts");
 Route::post('get-admin', "App\Http\Controllers\AdminController@getAdmin");
 // Route::post('get-page', "App\Http\Controllers\PageController@getPage");
 // Route::post('get-pages', "App\Http\Controllers\PageController@getPages");
