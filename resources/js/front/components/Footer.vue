eb4422064dc51ee6b9299a6a1c2967849c08b6d5
Nurlytan <nurik.bro.2000@mail.ru>
2023-05-15 03:45:49 +0600

    Call me and footer ready with new styles

--- resources/js/front/components/Footer.vue
+++ resources/js/front/components/Footer.vue
@@ -1,24 +1,23 @@
 <template>
-  <div class="wrapper-footer">
-    <div class="footer">
-      <span class="copy">© 2023 · ООО “Ковка и Дерево”</span>
-      <span>{{ number }}</span>
-      <span>{{email}}</span>
-      <span>{{address}}</span>
+    <div class="wrapper-footer">
+        <div class="footer">
+            <span class="copy">© 2023 · ООО “Ковка и Дерево”</span>
+            <span>{{ number }}</span>
+            <span>{{ email }}</span>
+            <span>{{ address }}</span>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-
 export default {
-  name: 'Footer',
-  data(){
+    name: "Footer",
+    data() {
         return {
             email: null,
             number: null,
             address: null,
-        }
+        };
     },
     mounted() {
         axios.post("/api/get-contacts", {}).then((response) => {
@@ -27,56 +26,76 @@ export default {
             this.address = response.data.contacts.address;
         });
     },
-}
+};
 </script>
 
 <style scoped lang="scss">
 .wrapper-footer {
-  width:100%;
-  border-top:1px solid #cccccc;
-  margin-top:100px;
-  height:50px;
-  background-color: #fff;
-  .footer {
-    max-width: var(--nav-max-width);
-    height: var(--nav-height);
-    padding: var(--nav-padding);
-    box-sizing: border-box;
-    margin: 10px auto;
-    background: inherit !important;
-    .copy {
-      float:left;
-      height:var(--span-navigation);
-      color:var(--color-black);
-      font-weight: var(--font-weight-btn-navigation);
-      font-family: var(--font-family);
-      font-size: var(--font-size-span-navigation);
-      padding:var(--padding-span-navigation);
-      box-sizing: border-box;
-      border-right:0 !important;
-      margin-top:3px;
+    width: 100%;
+    border-top: 1px solid #cccccc;
+    margin-top: 100px;
+    height: 50px;
+    background-color: #fff;
+    .footer {
+        max-width: var(--nav-max-width);
+        height: var(--nav-height);
+        padding: var(--nav-padding);
+        box-sizing: border-box;
+        margin: 10px auto;
+        @media (max-width: 830px) {
+            height: 72px;
+        }
+        .copy {
+            float: left;
+            height: var(--span-navigation);
+            color: var(--color-black);
+            font-weight: var(--font-weight-btn-navigation);
+            font-family: var(--font-family);
+            font-size: var(--font-size-span-navigation);
+            padding: var(--padding-span-navigation);
+            box-sizing: border-box;
+            border-right: 0 !important;
+            margin-top: 3px;
+            @media (max-width: 830px) {
+                margin-top: 19px;
+            }
+        }
+        span {
+            float: right;
+            height: var(--span-navigation);
+            color: var(--color-black);
+            font-weight: var(--font-weight-btn-navigation);
+            font-family: var(--font-family);
+            font-size: var(--font-size-span-navigation);
+            padding: var(--padding-span-navigation);
+            box-sizing: border-box;
+            border-right: var(--span-border-navigation);
+            margin-top: 3px;
+            &:nth-child(2) {
+                padding: var(--padding-span-child-navigation);
+                border-right: none !important;
+                font-family: var(--font-span-child-navigation);
+                font-size: var(--fontsize-span-child-navigation);
+            }
+            &:nth-child(4) {
+                color: var(--color-lastspan-child-navigation);
+            }
+            @media (max-width: 830px) {
+                border-right: none !important;
+                padding: 9px 20px 0 0;
+                &:nth-child(2) {
+                    padding: 6px 0 0 0;
+                    border-right: none !important;
+                    font-family: var(--font-span-child-navigation);
+                    font-size: var(--fontsize-span-child-navigation);
+                }
+                &:nth-child(4) {
+                    display: block;
+                    padding: 5px 0 0 20px;
+                    border-right: none !important;
+                }
+            }
+        }
     }
-    span {
-      float:right;
-      height:var(--span-navigation);
-      color:var(--color-black);
-      font-weight: var(--font-weight-btn-navigation);
-      font-family: var(--font-family);
-      font-size: var(--font-size-span-navigation);
-      padding:var(--padding-span-navigation);
-      box-sizing: border-box;
-      border-right:var(--span-border-navigation);
-      margin-top:3px;
-      &:nth-child(2) {
-        padding: var(--padding-span-child-navigation);
-        border-right: none !important;
-        font-family: var(--font-span-child-navigation);
-        font-size: var(--fontsize-span-child-navigation);
-      }
-      &:nth-child(4) {
-        color: var(--color-lastspan-child-navigation);
-      }
-    }
-  }
 }
 </style>
